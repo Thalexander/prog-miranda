ed00351b782c2bebf85b28c3b93f07250ac4fc12
Alexander Hale <thalexander@openmailbox.org>
2017-08-03 00:25:36 +0100

    Add answer to exercise 3.4

---
+++ ch3/3-4.m
@@ -0,0 +1,7 @@
+>|| Exercise 3.4
+>|| Explains what the numbertostring example function actually does
+
+> numbertostring :: num -> [char]
+> numbertostring n = "n"
+
+numbertostring ignores its input and returns the string "n" as the result in all cases
