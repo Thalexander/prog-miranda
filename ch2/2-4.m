19da12c7fedbe32e7143d0f7d1922aa2b5c7f785
Alexander Hale <thalexander@openmailbox.org>
2017-08-02 17:05:17 +0100

    Add solution to exercise 2.4

---
+++ ch2/2-4.m
@@ -0,0 +1,10 @@
+>|| Exercise 2.4
+>|| Modifies the solomonGrundy function so that Thursday and Friday are treated with special significance
+
+Based on the 19th Century nursery rhyme.
+
+> solomonGrundy "Monday"  = "Born"
+> solomonGrundy "Thursday" = "Took ill"
+> solomonGrundy "Friday"  = "Grew worse"
+> solomonGrundy "Sunday"  = "Died"
+> solomonGrundy anyday    = "Did something else"
