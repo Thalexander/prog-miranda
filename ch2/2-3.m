8ba8a52ecac2e06246a1e744f0665fdb48b9b6f9
Alexander Hale <thalexander@openmailbox.org>
2017-08-02 16:55:55 +0100

    Add solution to exercise 2.3

---
+++ ch2/2-3.m
@@ -0,0 +1,6 @@
+>|| Exercise 2.3
+>|| Provides a function dup which takes a single element of any type and returns a tuple with the elements duplicated
+
+Polymorphic function with type *->(*,*)
+
+> dup x = (x, x)
