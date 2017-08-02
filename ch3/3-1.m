97f7788b4fc580eed5f817e80842d07b388bab15
Alexander Hale <thalexander@openmailbox.org>
2017-08-03 00:07:20 +0100

    Add solution to exercise 3.1

---
+++ ch3/3-1.m
@@ -0,0 +1,10 @@
+>|| Exercise 3.1
+>|| Gives two possible correct versions of wrong_y
+
+> wrong_y = 1 : [2,3]
+
+Cons the number 1 onto the list of nums
+
+> wrong_y' = [1] : [[2,3]]
+
+Cons the list containing 1 onto the list of lists containing the list [2,3]
